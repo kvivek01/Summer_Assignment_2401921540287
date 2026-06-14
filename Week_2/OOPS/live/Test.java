9e9f62cee074765fc593c7b070a85df6681973e8
itsvivekkumar0705@gmail.com <kvivek68558@gmail.com>
2026-06-14 20:59:44 +0530

    Add OOPS Playable interface assignment

---
+++ Week_2/OOPS/live/Test.java
@@ -0,0 +1,23 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        Veena veena = new Veena();
+        veena.play();
+
+        Saxophone sax = new Saxophone();
+        sax.play();
+
+        Playable instrument;
+
+        instrument = veena;
+        instrument.play();
+
+        instrument = sax;
+        instrument.play();
+    }
+}
