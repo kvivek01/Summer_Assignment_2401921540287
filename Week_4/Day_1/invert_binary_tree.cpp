5f0b14bed1c8bc61aae023fcdc50acf3ec69908b
itsvivekkumar0705@gmail.com <kvivek68558@gmail.com>
2026-06-22 22:59:37 +0530

    Week_4 Day_1 Assignment Submitted

---
+++ Week_4/Day_1/invert_binary_tree.cpp
@@ -0,0 +1,12 @@
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == nullptr) return nullptr;
+
+        swap(root->left, root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
