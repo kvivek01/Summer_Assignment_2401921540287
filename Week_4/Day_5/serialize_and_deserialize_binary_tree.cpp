caf2cc9afa490b65cd467cea42b246d060a79a94
itsvivekkumar0705@gmail.com <kvivek68558@gmail.com>
2026-06-26 23:14:49 +0530

    Week_4 Day_5 Assignment Submitted

---
+++ Week_4/Day_5/serialize_and_deserialize_binary_tree.cpp
@@ -0,0 +1,69 @@
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+
+        if (!root) return "";
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        string ans = "";
+
+        while (!q.empty()) {
+
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node == nullptr) {
+                ans += "N,";
+                continue;
+            }
+
+            ans += to_string(node->val) + ",";
+
+            q.push(node->left);
+            q.push(node->right);
+        }
+
+        return ans;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+
+        if (data.empty()) return nullptr;
+
+        stringstream ss(data);
+        string str;
+
+        getline(ss, str, ',');
+        TreeNode* root = new TreeNode(stoi(str));
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+
+            TreeNode* node = q.front();
+            q.pop();
+
+            // Left child
+            getline(ss, str, ',');
+            if (str != "N") {
+                node->left = new TreeNode(stoi(str));
+                q.push(node->left);
+            }
+
+            // Right child
+            getline(ss, str, ',');
+            if (str != "N") {
+                node->right = new TreeNode(stoi(str));
+                q.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
